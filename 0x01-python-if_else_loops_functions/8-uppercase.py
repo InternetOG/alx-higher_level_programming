ad15038a772355e3c9b5634f2d17a155d8cbf449
InternetOG <omoleyeosadare@gmail.com>
2023-10-04 06:23:16 -0700

    edit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-	
+    for s in str:
+        if s is not None:
+            if s >= chr(97) and s <= chr(123):
+                print(chr(ord(s) - 32), end="")
+            else:
+                print(s, end="")
