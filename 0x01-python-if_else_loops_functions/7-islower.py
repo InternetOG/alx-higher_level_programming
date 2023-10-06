db9bc5e034cf2fe4b47804a34c71fa4e55c932d2
InternetOG <omoleyeosadare@gmail.com>
2023-10-06 16:39:25 -0700

    edit

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     if c is not None:
-        if c >= chr(97) and c <= chr(122):
+        if chr(97) <= c <= chr(122):
             return True
         else:
             return False
