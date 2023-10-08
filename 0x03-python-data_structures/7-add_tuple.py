cc420fe85a3ca23a0542ce411312667aec8de9c3
InternetOG <omoleyeosadare@gmail.com>
2023-10-08 20:43:51 +0100

    edit

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
+arr = []
+
+
 def add_tuple(tuple_a=(), tuple_b=()):
-    sumi = []
-    for x in range(len(tuple_a)) and range(len(tuple_b)):
-        sumi.append(tuple_a[x] + tuple_b[x])
-    return tuple(sumi)
+    if len(tuple_b) == 0:
+        return tuple_a
+    for x in range(len(tuple_b)):
+        if len(tuple_b) >= 2:
+            addTp = tuple_a[x] + tuple_b[x]
+            arr.append(addTp)
+        elif len(tuple_b) < 2 and len(tuple_b) == 1:
+            addTp = tuple_a[x] + tuple_b[x]
+            arr.append(addTp)
+            x += 1
+            addTp = tuple_a[x] + 0
+            arr.append(addTp)
+    return tuple(arr)
+
+
