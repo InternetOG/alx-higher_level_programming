5603744295e7c3a99b8716e5202bce1530c678e2
InternetOG <omoleyeosadare@gmail.com>
2023-10-08 17:43:07 +0100

    edit

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+def print_matrix_integer(matrix=[[]]):
+    for arr in matrix:
+        for num in range(len(arr)):
+            if num == 2:
+                print(arr[num], end="")
+            else:
+                print(arr[num], end=" ")
+        print(end="\n")
+
+
