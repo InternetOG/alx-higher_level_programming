f1113b7c475748bacc4a6ebfe9071267f4467dcd
InternetOG <omoleyeosadare@gmail.com>
2023-10-08 07:10:30 -0700

    edit

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,5 @@
 def print_list_integer(my_list=[]):
-    if my_list == []:
-        for num in my_list:
-            print("{:d}".format(num))
+    for num in my_list:
+        print("{:d}".format(num))
 
 
