640480c486bd390260507d0ee7fbc94937054543
InternetOG <omoleyeosadare@gmail.com>
2023-10-09 12:43:29 +0100

    edit

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    my_list_cpy = my_list.copy()
+    if idx < 0:
+        return my_list_cpy
+    elif idx > len(my_list):
+        return my_list_cpy
+    else:
+        my_list[idx] = element
+        return my_list
