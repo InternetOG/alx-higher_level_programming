11c24557230cc4e0730af9db07d0f6f8dfe56663
InternetOG <omoleyeosadare@gmail.com>
2023-10-09 15:49:22 +0100

    edit

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    newStr = ""
+    for char in my_string:
+        if char == 'c' or char == 'C':
+            continue
+        else:
+            newStr += char
+    return newStr
+
