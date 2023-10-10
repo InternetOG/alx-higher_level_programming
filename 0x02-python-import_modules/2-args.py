307cd7ecfa6458e9291e434619b208fdd23100ca
InternetOG <omoleyeosadare@gmail.com>
2023-10-10 16:09:55 +0100

    edit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
 
     argv = sys.argv
     if len(argv) < 2:
-        print("{}".format("0 arguments."))
+        print("0 arguments.")
     elif len(argv) > 1:
-        print("{} argument:".format((len(argv) - 1)))
+        print("{:d} argument:".format((len(argv) - 1)))
         for i in range(len(argv)):
             if i > 0:
-                print("{}: {}".format(i, argv[i]))
+                print("{:d}: {:d}".format(i, argv[i]))
