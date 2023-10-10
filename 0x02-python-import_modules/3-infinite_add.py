f4083262e69dd571f2a3d7c5fa405aab69f6c3a3
InternetOG <omoleyeosadare@gmail.com>
2023-10-10 17:23:32 +0100

    edit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
+def sumM(arr):
+    result = 0
+    for i in arr:
+        result += int(i)
+    return result
+
+
 if __name__ == "__main__":
     import sys
 
     argv = sys.argv
     result = 0
-
+    arr = []
     for i in range(len(argv)):
         if i > 0:
-            result += int(argv[i])
-    return result
+            arr.append(argv[i])
+    print("{}".format(sumM(arr)))
+
