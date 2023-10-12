ff306d121eecd02f76afb35bf1a13ede96318840
InternetOG <omoleyeosadare@gmail.com>
2023-10-12 10:04:06 +0100

    best_score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    arr = []
+    for k, v in a_dictionary.items():
+        arr.append(v)
+    maxNum = max(arr)
+    for k, v in a_dictionary.items():
+        if a_dictionary[k] == maxNum:
+            return k
+    else:
+        return None
