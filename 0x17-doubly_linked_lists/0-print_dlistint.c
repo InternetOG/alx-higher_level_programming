ed3697ef7ae1708240c2e21337cc1bd8ac57e9c9
InternetOG <omoleyeosadare@gmail.com>
2023-10-13 14:56:27 +0100

    print_dlistint

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,16 +7,16 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t n = 0;
+size_t n = 0;
 
-    if (h != NULL)
-    {
-        while (h != NULL)
-        {
-            n += 1;
-            h = h->next;
-        }
-        return n;
-    }
-    return n;
+if (h != NULL)
+{
+while (h != NULL)
+{
+n += 1;
+h = h->next;
+}
+return (n);
+}
+return (n);
 }
